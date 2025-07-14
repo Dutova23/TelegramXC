9e0c8b76f4d255e98d860a0ea1f60f483e1e58e7
Dutova23 <dutovaania@gmail.com>
2025-07-14 19:26:18 +0700

    Обновил app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Бот работает!'
+def home():
+    return "Бот работает!"
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
